757e8555c434fdeb9820fc807bcfbe100e154e17
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-17 22:20:08 -0400

    changes first migration to include timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_09_18_005910) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end

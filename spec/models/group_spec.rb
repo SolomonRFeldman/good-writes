6f5a34e29dd607e29baaec4ab29c625d17a797c8
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-19 13:48:31 -0400

    adds valid group and has many tests

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Group, :type => :model do
+  let(:valid_group) {
+    Group.create(
+      name: "Sunshine Friends",
+      form: "Poetry",
+      private: false
+    )
+  }
+
+  let(:user_1) { 
+    User.create(
+      :username => "Test",
+      :email => "Test@123.com",
+      :password => "123"
+    )
+  }
+
+  let(:user_2) { 
+    User.create(
+      :username => "Hello",
+      :email => "Hello@Hush.com",
+      :password => "hush"
+    )
+  }
+
+  it "is valid with name, form, and private set" do
+    expect(valid_group).to be_valid
+  end
+
+  it "has many users" do
+    valid_group.users << user_1
+    valid_group.users << user_2
+    expect(valid_group.users).to eq([user_1, user_2])
+  end
+
+
+  
+
+end

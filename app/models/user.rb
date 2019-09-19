654d1db4d9ac88cbe99df6c60770eb4c21d84867
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-19 14:23:20 -0400

    maps many to many between users and groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :user_groups
+  has_many :groups, through: :user_groups
+
   validates :username, :email, presence: true
   validates :email, uniqueness: { case_sensative: false }
   

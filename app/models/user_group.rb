be8c13718a333a008b79299c766e61a49270c98e
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-19 14:53:49 -0400

    defaults alias to user's username

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,4 +1,11 @@
 class UserGroup < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  before_save do
+    if self.alias == nil or self.alias == ""
+      self.alias = self.user.username
+    end
+  end
+
 end

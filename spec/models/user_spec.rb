f6171cf62c5df163876cc6179b2e229be60ee5d8
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-19 10:36:56 -0400

    changes second test object's info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,19 +18,23 @@ RSpec.describe User, :type => :model do
   end
 
   it "is invalid with a blank username" do
-    expect(User.new(username: "", email: "Test@123.com", password: "123")).to_not be_valid
+    expect(User.new(username: "", email: "Hello@Hush.com", password: "hush")).to_not be_valid
   end
 
   it "is invalid with a blank password" do
-    expect(User.new(username: "Test", email: "Test@123.com", password: "")).to_not be_valid
+    expect(User.new(username: "Hello", email: "Hello@Hush.com", password: "")).to_not be_valid
   end
 
   it "is invalid with a blank email" do
-    expect(User.new(username: "Test", email: "", password: "123")).to_not be_valid
+    expect(User.new(username: "Hello", email: "", password: "hush")).to_not be_valid
   end
 
   it "is invalid with a non-unique email" do
-    expect(User.new(username: "Test", email: "Test@123.com", password: "123")).to_not be_valid
+    expect(User.new(username: "Hello", email: "Test@123.com", password: "hush")).to_not be_valid
+  end
+
+  it "is invalid with a different case non-unique email" do
+    expect(User.new(username: "Hello", email: "test@123.com", password: "hush")).to_not be_valid
   end
 
 end

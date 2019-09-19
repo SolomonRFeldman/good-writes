ec614349bce330d800a5a7bf667c7832465261b6
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-19 14:52:32 -0400

    adds validation tests

--- spec/models/user_group_spec.rb
+++ spec/models/user_group_spec.rb
@@ -25,6 +25,30 @@ RSpec.describe UserGroup, :type => :model do
     expect(user_group.group).to eq(valid_group)
   end
 
+  it "cannot be made without a user_id" do
+    expect(UserGroup.new(group_id: valid_group.id)).to_not be_valid
+  end
+
+  it "cannot be made without a group id" do
+    expect(UserGroup.new(user_id: valid_user.id)).to_not be_valid
+  end
+
+  it "defaults alias to username" do
+    user_group = UserGroup.new
+    user_group.user = valid_user
+    user_group.group = valid_group
+    user_group.save
+    expect(user_group.alias).to eq(valid_user.username)
+  end
+
+  it "defaults moderator_status to false" do
+    user_group = UserGroup.new
+    user_group.user = valid_user
+    user_group.group = valid_group
+    user_group.save
+    expect(user_group.moderator_status).to eq(false)
+  end
+
 
   
 

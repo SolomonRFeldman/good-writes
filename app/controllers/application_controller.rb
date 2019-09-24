baa8e18da724c65586ee571526bf2deb10d10300
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-24 11:57:14 -0400

    fetches current user before every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :fetch_current_user
 
   def index
 
+  end
+  
+  private
+
+  def fetch_current_user
+    @current_user = User.find_by(id: session[:user_id])
   end
 
 end

3a0a8ac020f214dea0d14b275ea1cb869488785e
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-24 11:34:47 -0400

    sets up root and redirects it after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to root_path
     else
       redirect_to new_user_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'application#index'
+  
   resources :users
 
 end

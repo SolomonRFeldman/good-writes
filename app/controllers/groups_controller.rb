5f1280e35471455efdb118321a3f90035ad65cfa
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-26 15:43:16 -0400

    links user to group on creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
   def create
     if @current_user
       @group = Group.create(group_params)
+      UserGroup.create(user_id: @current_user.id, group_id: @group.id)
       redirect_to group_path(@group)
     end
   end

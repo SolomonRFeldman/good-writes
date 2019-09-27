c7d2b805456427ccdfdc30b4b8960565a7973b0d
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-26 21:39:29 -0400

    adds test for leaving group

--- spec/features/group_spec.rb
+++ spec/features/group_spec.rb
@@ -82,4 +82,14 @@ describe 'Group Features', :type => :feature do
     expect(page.body[/<\/header>[\S\s]+/]).to include("Test")
   end
 
+  it "can have a user leave" do
+    valid_user
+    page.set_rack_session(user_id: valid_user.id)
+    valid_group
+    UserGroup.create(user_id: valid_user.id, group_id: valid_group.id)
+    visit group_path(valid_group)
+    click_button('Leave Group')
+    expect(valid_user.groups).to be_empty
+  end
+
 end

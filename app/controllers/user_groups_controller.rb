83dc038709692da41c58169bf4d7f64f850c1f19
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-27 09:21:06 -0400

    allows users to change their alias

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,8 +1,24 @@
 class UserGroupsController < ApplicationController
 
+  def edit
+    @user_group = UserGroup.find_by(id: params[:id]) if @current_user
+  end
+
+  def update
+    @user_group = UserGroup.find_by(id: params[:id])
+    @user_group.update(user_group_params)
+    redirect_to group_path(@user_group.group_id)
+  end
+
   def destroy
     UserGroup.find_by(id: params[:id]).destroy
     redirect_to root_path
+  end
+
+  private
+
+  def user_group_params
+    params.require(:user_group).permit(:alias)
   end
 
 end

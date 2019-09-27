4f1598f5404425b3564366213ab260160ff2b068
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-27 11:33:54 -0400

    reroutes tests for nested user_group route

--- spec/features/group_spec.rb
+++ spec/features/group_spec.rb
@@ -97,7 +97,7 @@ describe 'Group Features', :type => :feature do
     page.set_rack_session(user_id: valid_user.id)
     valid_group
     user_group = UserGroup.create(user_id: valid_user.id, group_id: valid_group.id)
-    visit edit_user_group_path(user_group)
+    visit edit_group_user_group_path(valid_group, user_group)
     fill_in("user_group[alias]", with: "Victor")
     click_button('Change Alias')
     expect(page).to have_content("Victor")
@@ -110,7 +110,7 @@ describe 'Group Features', :type => :feature do
     user_group = UserGroup.create(user_id: valid_user.id, group_id: valid_group.id)
     secondary_user
     page.set_rack_session(user_id: secondary_user.id)
-    visit edit_user_group_path(user_group)
+    visit edit_group_user_group_path(valid_group, user_group)
     expect(current_path).to eq('/')
   end
 

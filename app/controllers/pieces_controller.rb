32b8e5c3cee62c9522bdc83b269ba4e7d16abd6e
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-30 10:17:56 -0400

    adds feature to edit pieces

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -12,6 +12,17 @@ class PiecesController < ApplicationController
     end
   end
 
+  def edit
+    @piece = Piece.find_by(id: params[:id])
+    redirect_to root_path unless @piece && (session[:user_id] == @piece.user_id)
+  end
+
+  def update
+    @piece = Piece.find_by(id: params[:id])
+    @piece.update(piece_params) if @piece && (session[:user_id] == @piece.user_id)
+    redirect_to user_path(@current_user)
+  end
+
   private
  
   def piece_params

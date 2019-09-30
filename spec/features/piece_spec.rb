d20a8f4e3257111799f1a8c10bf11e7e2a9f66a2
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-30 10:16:53 -0400

    adds piece editing tests

--- spec/features/piece_spec.rb
+++ spec/features/piece_spec.rb
@@ -16,4 +16,16 @@ describe 'Piece Features', :type => :feature do
     expect(Piece.all.last.title).to eq("My Little Poem")
   end
 
+  it "can be edited" do
+    valid_user
+    page.set_rack_session(user_id: valid_user.id)
+    valid_piece.update(user_id: valid_user.id)
+    visit user_path(valid_user)
+    click_button('Edit')
+    fill_in("piece[title]", with: "My Big Poem")
+    click_button('Update')
+    expect(page).to have_content("My Big Poem")
+    expect(Piece.all.last.title).to eq("My Big Poem")
+  end
+
 end

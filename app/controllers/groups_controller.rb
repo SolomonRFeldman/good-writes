4526fc1353e1eaec2d0dc7bfaa11806e48235d0d
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-10-02 14:09:44 -0400

    sets creater as a mod

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ class GroupsController < ApplicationController
   def create
     if @current_user
       @group = Group.create(group_params)
-      UserGroup.create(user_id: @current_user.id, group_id: @group.id, alias: user_group_params[:alias])
+      UserGroup.create(user_id: @current_user.id, group_id: @group.id, alias: user_group_params[:alias], moderator_status: true)
       redirect_to group_path(@group)
     end
   end

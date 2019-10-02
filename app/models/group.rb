13e23f998d382482cc572ffbc350bd5f90c30e60
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-10-02 14:56:30 -0400

    adds featured piece method

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,8 @@ class Group < ApplicationRecord
   has_many :comments
   belongs_to :piece, optional: true
 
+  def featured_piece
+    user_groups.order(:created_at)[self.point_in_cycle].piece
+  end
+
 end

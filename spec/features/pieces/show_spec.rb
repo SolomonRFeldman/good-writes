8dad94884735c57b0f6b19407cef5bd010d17f25
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-10-05 08:48:13 -0400

    adds edit/delete buttons on piece show

--- spec/features/pieces/show_spec.rb
+++ spec/features/pieces/show_spec.rb
@@ -40,4 +40,32 @@ describe 'Piece Features', :type => :feature do
     end
   end
 
+  context "when a user clicks the edit button on their piece's show" do
+    let(:user_piece) { create(:valid_piece, user_id: valid_user.id) }
+    before do
+      valid_user
+      user_piece
+      visit user_piece_path(valid_user, user_piece)
+      click_button 'Edit'
+    end
+
+    it "navigates to the piece's edit page" do
+      expect(page.current_path).to eq(edit_user_piece_path(valid_user, user_piece))
+    end
+  end
+
+  context "when a user clicks the delete button on their piece's show" do
+    let(:user_piece) { create(:valid_piece, user_id: valid_user.id) }
+    before do
+      valid_user
+      user_piece
+      visit user_piece_path(valid_user, user_piece)
+      click_button 'Delete'
+    end
+
+    it "deletes the piece" do
+      expect(Piece.find_by(id: user_piece.id)).to be_nil
+    end
+  end
+
 end

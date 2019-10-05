051723f9ab75441bd9eabe2d8b8a711f4eb03cad
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-10-05 17:18:43 -0400

    removes unessasary object creation

--- spec/features/pieces/show_spec.rb
+++ spec/features/pieces/show_spec.rb
@@ -41,30 +41,24 @@ describe 'Piece Features', :type => :feature do
   end
 
   context "when a user clicks the edit button on their piece's show" do
-    let(:user_piece) { create(:valid_piece, user_id: valid_user.id) }
     before do
-      valid_user
-      user_piece
-      visit user_piece_path(valid_user, user_piece)
+      visit user_piece_path(valid_user, valid_piece)
       click_button 'Edit'
     end
 
     it "navigates to the piece's edit page" do
-      expect(page.current_path).to eq(edit_user_piece_path(valid_user, user_piece))
+      expect(page.current_path).to eq(edit_user_piece_path(valid_user, valid_piece))
     end
   end
 
   context "when a user clicks the delete button on their piece's show" do
-    let(:user_piece) { create(:valid_piece, user_id: valid_user.id) }
     before do
-      valid_user
-      user_piece
-      visit user_piece_path(valid_user, user_piece)
+      visit user_piece_path(valid_user, valid_piece)
       click_button 'Delete'
     end
 
     it "deletes the piece" do
-      expect(Piece.find_by(id: user_piece.id)).to be_nil
+      expect(Piece.find_by(id: valid_piece.id)).to be_nil
     end
   end
 

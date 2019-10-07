62a01f7a7dba9fbbe9fc01f8c272e1cce10b0f73
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-10-07 12:26:07 -0400

    refactors featured piece to only do one ping to db

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,12 +5,32 @@ class Group < ApplicationRecord
   belongs_to :piece, optional: true
 
   def featured_piece
-    if user_group = user_groups.order(:created_at)[self.point_in_cycle]
-      user_group.piece
+    if piece = featured_piece_search[self.point_in_cycle]
+      piece if piece.id
     else
       update(point_in_cycle: 0)
-      user_groups.order(:created_at)[self.point_in_cycle].piece
+      featured_piece_search[self.point_in_cycle]
     end
+  end
+
+  private
+
+  def featured_piece_search
+    Piece.joins("RIGHT JOIN user_groups ON user_groups.piece_id = pieces.id")
+    .select(featured_piece_select)
+    .order("user_groups.created_at")
+    .where("user_groups.group_id = ?", self.id)
+  end
+
+  def featured_piece_select
+    <<~SQL
+      user_groups.alias,
+      pieces.id,
+      pieces.user_id,
+      pieces.title,
+      pieces.form,
+      pieces.content
+    SQL
   end
 
 end

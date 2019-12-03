ced75a53bd1ae018bad65f1aa6b04ea46a78613b
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-03 12:22:13 -0500

    adds navbar brand

--- good-writes-client/src/components/NavBar/NavBar.js
+++ good-writes-client/src/components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ export default class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href='/'>Good Writes</a>
       </nav>
     )
   }

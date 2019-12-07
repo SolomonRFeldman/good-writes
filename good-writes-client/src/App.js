8a974bcf145c3c344b9d7811ed537283ccc0a6be
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-07 01:59:17 -0500

    fetches users info from token on load

--- good-writes-client/src/App.js
+++ good-writes-client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
+import { connect } from 'react-redux'
 
-function App() {
+
+
+function App(props) {
+
+  useEffect(() => {
+    if(localStorage.token) {
+      const configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({ token: localStorage.token })
+      }
+      
+      fetch('/login', configObj).then(response => response.json()).then(json => {
+        props.addCurrentUser(json)
+        localStorage.token = json.token
+      })
+    }
+  })
+
   return (
     <div className="App">
       <header>
@@ -13,4 +35,6 @@ function App() {
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({ addCurrentUser: payload => { dispatch({ type: "ADD_CURRENT_USER", payload }) } })
+
+export default connect(null, mapDispatchToProps)(App)

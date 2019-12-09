b2f69a5d8531973cf04751a1fa1feb46824c67f2
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-09 18:30:28 -0500

    adds piece show with piece fetch

---
+++ good-writes-client/src/components/Pieces/PieceShow.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import Piece from './Piece'
+
+export default function PieceShow(props) {
+
+  const [piece, setPiece] = useState()
+  useEffect( () => {
+    const configObj = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Token": localStorage.token
+      },
+    }
+    fetch(`/pieces/${props.match.params.id}`, configObj).then(response => response.json()).then(json => setPiece(json))
+  }, [])
+
+  return(
+    <Piece varient='show' piece={piece} />
+  )
+
+}

ff7a420040fc40f542d3d7210221e79dba564678
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-09 17:24:31 -0500

    adds link to piece show page

--- good-writes-client/src/components/Pieces/Piece.js
+++ good-writes-client/src/components/Pieces/Piece.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom';
+
 
 export default function Piece(props) {
+
+
+  const header = <h1 className='display-4 d-inline-block'>{props.piece.title}</h1>
+
+  const headerElement = () => {
+    if(props.varient === 'profile') { return (
+      <Link to={`/users/${props.piece.user_id}/pieces/${props.piece.id}`}>{header}</Link>
+    )} else {
+      return header
+    }
+  }
 
   const bodyStyle = () => {
     if(props.varient === 'profile') return {overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}
@@ -10,7 +23,7 @@ export default function Piece(props) {
   return(
     <Card style={{width: '800px'}} className='mx-auto my-4'>
       <Card.Header>
-        <h1 className='display-4 d-inline-block'>{props.piece.title}</h1>
+        {headerElement()}
         <h3 className='d-inline-block ml-4'>({props.piece.form})</h3>
       </Card.Header>
       <Card.Body>

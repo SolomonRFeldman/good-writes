e4acb5cdc65be8b77efe75506ddbc50d829d08b6
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-09 16:33:37 -0500

    adds user fetch and displays pieces on user show

--- good-writes-client/src/components/Users/User.js
+++ good-writes-client/src/components/Users/User.js
@@ -1,11 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import Pieces from '../Pieces/Pieces'
 
 export default function User(props) {
 
-  useEffect(() => console.log(props))
+  const [user, setUser] = useState({ id: props.match.params.id, username: '', email: '', pieces: [], groups: [] })
+
+  useEffect(() => {
+    const configObj = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Token": localStorage.token
+      },
+    }
+    fetch(`/users/${user.id}`, configObj).then(response => response.json()).then(json => setUser(json))
+  }, [])
+
+  useEffect(() => console.log(user))
 
   return(
-    <h1>{props.match.params.id}</h1>
+    <React.Fragment>
+      <h1 className="display-3 text-center">Your Pieces</h1>
+      <Pieces pieces={user.pieces} varient={'profile'} />
+    </React.Fragment>
   )
 
 }

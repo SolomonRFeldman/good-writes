1200fd98f774ba9353b0878551b2b15dcada5d80
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-09 19:56:39 -0500

    adds new piece form layout

--- good-writes-client/src/components/Pieces/NewPieceForm.js
+++ good-writes-client/src/components/Pieces/NewPieceForm.js
@@ -1,10 +1,51 @@
 import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal'
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export default function NewPieceForm(props) {
+
+  const [formData, setFormData] = useState({ title: '', form: 'Poetry', content: '' })
+  const handleChange = event => setFormData({ ...formData, [event.target.id]: event.target.value })
+
+  const handleSubmit = () => {}
+
   return (
-    <Modal show={props.show} onHide={props.handleClose} centered>
-      
+    <Modal size='lg' show={props.show} onHide={props.handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Your Piece</Modal.Title>
+      </Modal.Header>
+
+      <Form onSubmit={handleSubmit}>
+
+        <Modal.Body>
+            <Form.Group as={Row} >
+              <Form.Label className='ml-3' column sm={0}>Title</Form.Label>
+              <Col>
+                <Form.Control id='title' onChange={handleChange} type='text' placeholder='Title' value={formData.title} />
+              </Col>
+              <Form.Label column sm={0}>Form</Form.Label>
+              <Col>
+                <Form.Control id='form' onChange={handleChange} as='select' value={formData.form}>
+                  <option>Poetry</option>
+                  <option>Fiction</option>
+                  <option>Non-Fiction</option>
+                </Form.Control>
+              </Col>
+            </Form.Group>
+            <Form.Group >
+              <Form.Label>Content</Form.Label>
+              <Form.Control id='content' onChange={handleChange} as='textarea' rows='18' value={formData.content} />
+            </Form.Group>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button type='submit' varient='primary'>Log In</Button>
+        </Modal.Footer>
+
+      </Form>
     </Modal>
   )
 

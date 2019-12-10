97eb9c84b0675494aebbb24351fff01d06cce5f1
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-09 20:12:51 -0500

    adds route and controller action for create piece

--- good-writes-server/app/controllers/pieces_controller.rb
+++ good-writes-server/app/controllers/pieces_controller.rb
@@ -1,10 +1,25 @@
 class PiecesController < ApplicationController
 
+  def create
+    if user_id = JwtService.decode(request.headers[:Token])[:user_id]
+      piece = Piece.new(piece_params.merge({user_id: user_id}))
+      if piece.save
+        render json: piece.to_json
+      end
+    end
+  end
+
   def show
     piece = Piece.find_by(id: params[:id]) 
     if JwtService.decode(request.headers[:Token])[:user_id] == piece.user_id
       render json: piece.to_json
     end
+  end
+
+  private
+ 
+  def piece_params
+    params.require(:piece).permit(:title, :content, :form)
   end
 
 end
--- good-writes-server/config/routes.rb
+++ good-writes-server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:show]
-  resources :pieces, only: [:show]
+  resources :pieces, only: [:show, :create]
   resources :groups, only: [:index]
 
   post '/signup' => 'users#create'

eb31af57ae6384aea72bf06205d23874ffff7d98
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-11 11:58:21 -0500

    show button redirects to group page

--- good-writes-client/src/components/Groups/ShowGroupButton.js
+++ good-writes-client/src/components/Groups/ShowGroupButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 export default function ShowGroupButton(props) {
   
   return(
-    <Button className={props.className} variant='primary'>View</Button>
+    <Link to={`/groups/${props.group.id}`}><Button className={props.className} variant='primary'>View</Button></Link>
   )
 
 }

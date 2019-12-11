aa0187fc76bc204f8d36ac260836e801f4603345
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-11 11:59:57 -0500

    replaces redux redirect with wrapping router link

--- good-writes-client/src/components/Session/LogOutButton.js
+++ good-writes-client/src/components/Session/LogOutButton.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function LogOutButton(props) {
 
   const handleClick = () => {
-    props.triggerRedirect('/')
     props.removeCurrentUser()
     localStorage.clear()
   }
   
   return(
-    <Button onClick={handleClick} className={props.className} variant='secondary'>Log Out</Button>
+    <Link to='/'><Button onClick={handleClick} className={props.className} variant='secondary'>Log Out</Button></Link>
   )
 
 }
 
 const mapDispatchToProps = dispatch => ({ 
-  removeCurrentUser: payload => { dispatch({ type: "REMOVE_CURRENT_USER", payload }) },
-  triggerRedirect: path => { dispatch({ type: 'TRIGGER_REDIRECT', path }) }
+  removeCurrentUser: payload => { dispatch({ type: "REMOVE_CURRENT_USER", payload }) }
 })
 
 export default connect(null, mapDispatchToProps)(LogOutButton)

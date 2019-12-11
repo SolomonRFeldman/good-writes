d3d713448e7d741fe62204be6f9ed7d711d0e602
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-11 02:37:56 -0500

    returns user's user_groups if logged in

--- good-writes-server/app/controllers/groups_controller.rb
+++ good-writes-server/app/controllers/groups_controller.rb
@@ -1,7 +1,8 @@
 class GroupsController < ApplicationController
 
   def index
-    render json: { groups: Group.all.map{ |group| group.attributes } }
+    joined_groups = UserGroup.where(user_id: JwtService.decode(request.headers[:Token])[:user_id]) if request.headers[:Token]
+    render json: { groups: Group.all.map{ |group| group.attributes }, user_groups: joined_groups }
   end
 
 end

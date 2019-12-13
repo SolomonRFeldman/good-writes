17423ed304f6761879a08e93dcc0dc6604950526
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-13 08:14:41 -0500

    removes ability to comment on own piece

--- good-writes-client/src/components/Groups/FeaturedPiece.js
+++ good-writes-client/src/components/Groups/FeaturedPiece.js
@@ -7,12 +7,18 @@ export default function FeaturedPiece(props) {
   const [comments, setComments] = useState([])
   useEffect(() => setComments(props.featured_piece.comments), [props.featured_piece.comments])
 
+  const canComment = () => {
+    if(props.featured_piece.user_group_id !== props.userGroup.id) {
+      return <NewCommentForm userGroup={props.userGroup} piece={props.featured_piece} comments={comments} setComments={setComments} />
+    }
+  }
+
   return(
     <React.Fragment>
       <h2 className="text-center">{props.featured_piece.alias}'s Piece: </h2>
       <Piece variant={'group-show'} piece={props.featured_piece} />
       <Comments comments={comments} setComments={setComments} />
-      <NewCommentForm userGroup={props.userGroup} piece={props.featured_piece} comments={comments} setComments={setComments} />
+      {canComment()}
     </React.Fragment>
   )
 

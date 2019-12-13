589dc69ede5bbc59d609e444b6d8b225025092a9
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-13 08:04:39 -0500

    routes and adds action for comment creation

---
+++ good-writes-server/app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    @user_group = UserGroup.find_by(id: params[:user_group][:id])
+    if authorized?(@user_group)
+      render json: Comment.create(create_comment_params).to_json
+    end
+  end
+
+  private
+
+  def create_comment_params
+    params
+      .require(:comment)
+      .permit(:content, :piece_id)
+      .merge(user_id: @current_user_id, group_id: @user_group[:group_id], author_alias: @user_group[:alias])
+  end
+
+end
--- good-writes-server/config/routes.rb
+++ good-writes-server/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :pieces, only: [:show, :create, :update, :destroy]
   resources :groups, only: [:index, :show, :create, :update]
   resources :user_groups, only: [:create, :update, :destroy]
+  resources :comments, only: [:create]
 
   post '/signup' => 'users#create'
   post '/login' => 'sessions#create'

ffbcb49206a197da3005d49e7d4b27aaa2cd0ddc
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-19 02:57:02 -0500

    removes redirect reducer

--- good-writes-client/src/index.js
+++ good-writes-client/src/index.js
@@ -5,14 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import manageCurrentUser from './reducers/manageCurrentUser';
-import manageRedirect from './reducers/manageRedirect';
 
 const store = createStore(
-  combineReducers({ currentUser: manageCurrentUser, redirect: manageRedirect }), 
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  combineReducers({ currentUser: manageCurrentUser }), 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() && applyMiddleware(thunk)
 )
 
 ReactDOM.render(

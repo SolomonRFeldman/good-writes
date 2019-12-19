2637d96a6e53b07b2b50abb03b02af494aadd9da
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-19 04:44:34 -0500

    removes unessesary instance variables

--- good-writes-server/app/controllers/sessions_controller.rb
+++ good-writes-server/app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     if params[:token]
-      token = params[:token] if @user = User.find_by(id: JwtService.decode(params[:token])[:user_id])
+      token = params[:token] if user = User.find_by(id: JwtService.decode(params[:token])[:user_id])
     else
-      @user = User.find_by(email: params[:user][:email])
-      unless @user && @user.authenticate(params[:user][:password])
+      user = User.find_by(email: params[:user][:email])
+      unless user && user.authenticate(params[:user][:password])
         return render json: { error: true }
       end
     end
-    render json: @user.login_hash(token)
+    render json: user.login_hash(token)
   end
 
 end

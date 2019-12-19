a0d200199983e3ebb26d60bffed5ba13f4153f76
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-19 04:50:24 -0500

    removes navbar redirect from redux

--- good-writes-client/src/components/NavBar/NavBar.js
+++ good-writes-client/src/components/NavBar/NavBar.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import LogInBanner from './LogInBanner';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 
-function NavBar(props) {
-
-  const redirect = () => { if(props.redirect) {
-    const path = props.redirect
-    props.resetRedirectTrigger()
-    return <Redirect push to={path} /> 
-  }}
+export default function NavBar(props) {
 
   return (
     <Navbar className={props.className} bg='dark' variant='dark'>
       <Navbar.Brand as={Link} to='/'>Good Writes</Navbar.Brand>
       <LogInBanner className='ml-auto' />
-      {redirect()}
     </Navbar>
   );
 
 }
-
-const mapStateToProps = ({ redirect }) => ({ redirect })
-const mapDispatchToProps = dispatch => ({ resetRedirectTrigger: payload => { dispatch({ type: 'RESET_REDIRECT_TRIGGER', payload }) } })
-
-export default connect(mapStateToProps, mapDispatchToProps)(NavBar)

c394bbeabb5c012c69da06cdd59680c9ec669c69
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-21 07:37:20 -0500

    shortens requests to shorthand functions

--- good-writes-client/src/fetchRequests.js
+++ good-writes-client/src/fetchRequests.js
@@ -9,22 +9,11 @@ const configObj = (method) => {
   return localStorage.token ? {...obj, headers: { ...obj.headers, "Token": localStorage.token }} : obj
 }
 
-export const getRequest = (url) => {
-  return fetch(url, configObj("GET")).then(response => response.json())
-}
-
-export const deleteRequest = (url) => {
-  return fetch(url, configObj("DELETE")).then(response => response.json())
-}
+export const getRequest = (url) => fetch(url, configObj("GET")).then(response => response.json())
+export const deleteRequest = (url) => fetch(url, configObj("DELETE")).then(response => response.json())
 
 const configObjWithBody = (method, body) => {
   return { ...configObj(method), body: JSON.stringify(body) }
 }
-
-export const postRequest = (url, body) => {
-  return fetch(url, configObjWithBody("POST", body)).then(response => response.json())
-}
-
-export const patchRequest = (url, body) => {
-  return fetch(url, configObjWithBody("PATCH", body)).then(response => response.json())
-}
+export const postRequest = (url, body) => fetch(url, configObjWithBody("POST", body)).then(response => response.json())
+export const patchRequest = (url, body) => fetch(url, configObjWithBody("PATCH", body)).then(response => response.json())

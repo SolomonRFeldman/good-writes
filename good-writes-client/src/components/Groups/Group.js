415fa7011f5be3c3f6e09063e870cd204d02759f
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-08 16:14:06 -0500

    puts buttons in separate grid to prevent manipulation on overflow

--- good-writes-client/src/components/Groups/Group.js
+++ good-writes-client/src/components/Groups/Group.js
@@ -6,7 +6,7 @@ import GroupButtons from './GroupButtons';
 export default function Group(props) {
 
   const buttons = () => { 
-    if(props.userGroups) return <GroupButtons className='my-auto buttons' group={props.group} userGroups={props.userGroups} /> 
+    if(props.userGroups) return <GroupButtons className='my-auto mx-auto buttons' group={props.group} userGroups={props.userGroups} /> 
   }
 
   return (
@@ -14,8 +14,8 @@ export default function Group(props) {
       <Card.Header className='header'>
         <div className='label'>
           <h1 className="display-4 d-inline-block mr-2">{props.group.name}</h1>
+          <h3 className="d-inline-block">( {props.group.form} )</h3>
         </div>
-          <span className="type">{props.group.form}</span>
           {buttons()}
       </Card.Header>
     </Card>
